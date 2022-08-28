ba26a932f3fca393ce77992de0f1c81081c94a5e
jomulagy <jomulagy688@gmail.com>
2022-08-28 17:20:05 +0900

    .

--- gradusmu/datas/views.py
+++ gradusmu/datas/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
 
-# Create your views here.
+# home
+def home():
+    return 
+
+# 개인정보 가져오기
+def inform():
+    return
+
+# 학점 가져오기
+# 총학점, 전공학점, 교양학점
+def point():
+    return
+
+#수강한 과목 가져오기
+#상위항목, 하위항목별로 구분 가능해야됨
+def complete():
+    return
+
+#과목 상세
+def subject_detail():
+    return 
+
+#학점 상세 페이지
+def point_detail():
+    return
+
+#과목 추가
+def put_subject():
+    return
