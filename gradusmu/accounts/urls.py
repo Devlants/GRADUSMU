d0e1c06d4697c9b787289b49115f56f44b5ea0df
jomulagy <jomulagy688@gmail.com>
2022-09-01 03:24:31 +0900

    회원정보 수정

--- gradusmu/accounts/urls.py
+++ gradusmu/accounts/urls.py
@@ -8,7 +8,8 @@ accounts_patterns = [
     path('find_id/', views.find_id, name='find_id'),
     path('find_pwd/send_email', views.find_pwd_send_email, name='find_pwd_send_email'),
     path('find_pwd/change_pwd', views.find_pwd_change_pwd, name='find_pwd_change_pwd'),
-    path('profile/',views.profile,name = "profile"),
+    path('profile/',views.profile,name = "settings"),
+    #path('profile_img/',views.profile_img,name = "profile_img"),
 ]
 
 urlpatterns = [
--- gradusmu/config/urls.py
+++ gradusmu/config/urls.py
@@ -2,6 +2,9 @@
 from django.contrib import admin
 from django.urls import path, include
 from accounts import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
@@ -9,3 +12,4 @@ urlpatterns = [
     path("",views.home,name = 'home'),
     
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
