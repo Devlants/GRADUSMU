b51ce1b9e3de648c33a24f2950c74e62aa1addac
Walloonam <jh37106@gmail.com>
2022-09-02 04:59:06 +0900

    여러 수정

--- gradusmu/datas/urls.py
+++ gradusmu/datas/urls.py
@@ -8,7 +8,10 @@ datas_patterns = [
     path("subject_detail/",views.subject_detail,name = "subject_detail"),
     path("score_detail/",views.score_detail,name = "score_detail"),
     path("score_graph_detail/",views.score_graph_detail,name = "scoreGraphDetail"),
-    path("balanced_culture/",views.balanced_search_subject,name = "balanced_culture")
+    path("balanced_culture/",views.balanced_search_subject,name = "balanced_culture"),
+    path("core_culture/", views.core_search_subject, name="core_culture"),
+    path("ess_culture/", views.ess_search_subject, name="ess_culture")
+
 ]
 
 urlpatterns = [
